0c48e6506a5b09db8eb0f1dc26ea8df0b87e87e9
Caitlin Haughey <Caitlin@Caitlings-MacBook-Air.local>
2018-02-06 18:25:57 +0100

    .

--- Search/search.php
+++ Search/search.php
@@ -18,14 +18,14 @@ include 'db.php';
         $bb_id_criteria = mysqli_real_escape_string($link, $_REQUEST['bb_id_criteria']);
         $comment_criteria = mysqli_real_escape_string($link, $_REQUEST['comment_criteria']);
         $creation_year_criteria = mysqli_real_escape_string($link, $_REQUEST['creation_year_criteria']);
-        $inserted_date_criteria = mysqli_real_escape_string($link, $_REQUEST['inserted_year_criteria']);
+        $inserted_date_criteria = mysqli_real_escape_string($link, $_REQUEST['inserted_date_criteria']);
     
         // query the database table
-        $sql="SELECT * FROM entry WHERE (ID like '%".$id_criteria."%') OR (backbone like '%".$backbone_criteria."%') OR 
-        (strain like '%".$strain_criteria."%') OR (insert like '%".$insert_criteria."%') OR 
-        (bb_id like '%".$bb_id_criteria."%') OR (comment like '%".$comment_criteria."%') OR
-        (created_yearlike '%".$creation_year_criteria."%') OR
-        (inserted_year like '%".$inserted_date_criteria."%')";
+        $sql="SELECT * FROM entry WHERE (id like '%".$id_criteria."%') OR (backbone like '%".$backbone_criteria."%') OR 
+        (strain like '%".$strain_criteria."%') OR (ins like '%".$insert_criteria."%') OR 
+        (entry_reg like '%".$bb_id_criteria."%') OR (comment like '%".$comment_criteria."%') OR
+        (year_created like '%".$creation_year_criteria."%') OR
+        (date_db like '%".$inserted_date_criteria."%')";
             
         $result=mysql_query($sql);
 
