2912dfd6ffc39fb59a00a2d10b2e88e39b235782
WG-LAPTOP\Wiktor <wiktorg.95@gmail.com>
2018-02-06 11:14:45 +0100

    no message

--- upstrain.sql
+++ upstrain.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 05, 2018 at 03:24 PM
+-- Generation Time: Feb 06, 2018 at 10:14 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -33,17 +33,14 @@ CREATE TABLE IF NOT EXISTS `backbone` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `Bb_reg` varchar(50) DEFAULT NULL,
+  `date_db` int(10) NOT NULL,
+  `year_created` int(4) NOT NULL,
+  `creator` int(3) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `bb_name` (`name`),
-  UNIQUE KEY `bb_regname` (`Bb_reg`)
+  UNIQUE KEY `bb_regname` (`Bb_reg`),
+  KEY `bb_creat_id` (`creator`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `backbone`
---
-
-INSERT INTO `backbone` (`id`, `name`, `Bb_reg`) VALUES
-(1, 'test', 'test');
 
 -- --------------------------------------------------------
 
@@ -70,14 +67,7 @@ CREATE TABLE IF NOT EXISTS `entry` (
   KEY `insert_id` (`ins`),
   KEY `strain_id` (`strain`),
   KEY `creator_id` (`creator`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `entry`
---
-
-INSERT INTO `entry` (`id`, `comment`, `year_created`, `date_db`, `entry_reg`, `sequence`, `backbone`, `strain`, `ins`, `creator`) VALUES
-(002, 'test', 2018, 20180205, 'test', 0, 1, 1, 1, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Triggers `entry`
@@ -98,15 +88,9 @@ DROP TABLE IF EXISTS `entry_upstrain`;
 CREATE TABLE IF NOT EXISTS `entry_upstrain` (
   `entry_id` int(3) UNSIGNED ZEROFILL NOT NULL,
   `upstrain_id` varchar(10) NOT NULL,
+  PRIMARY KEY (`upstrain_id`),
   UNIQUE KEY `id_link` (`entry_id`,`upstrain_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `entry_upstrain`
---
-
-INSERT INTO `entry_upstrain` (`entry_id`, `upstrain_id`) VALUES
-(002, 'UU2018002');
 
 -- --------------------------------------------------------
 
@@ -120,17 +104,14 @@ CREATE TABLE IF NOT EXISTS `ins` (
   `name` varchar(50) NOT NULL,
   `type` varchar(50) NOT NULL,
   `ins_reg` varchar(50) NOT NULL,
+  `creator` int(3) NOT NULL,
+  `year_created` int(4) NOT NULL,
+  `date_db` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `insert_name` (`name`),
-  UNIQUE KEY `ins_regname` (`ins_reg`)
+  UNIQUE KEY `ins_regname` (`ins_reg`),
+  KEY `creator_id` (`creator`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ins`
---
-
-INSERT INTO `ins` (`id`, `name`, `type`, `ins_reg`) VALUES
-(1, 'test', 'test', 'test');
 
 -- --------------------------------------------------------
 
@@ -156,6 +137,21 @@ INSERT INTO `strain` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `upstrain_file`
+--
+
+DROP TABLE IF EXISTS `upstrain_file`;
+CREATE TABLE IF NOT EXISTS `upstrain_file` (
+  `upstrain_id` varchar(10) NOT NULL,
+  `name_original` varchar(50) NOT NULL,
+  `name_new` varchar(20) GENERATED ALWAYS AS (concat(`upstrain_id`,'.fasta')) VIRTUAL,
+  PRIMARY KEY (`upstrain_id`),
+  UNIQUE KEY `name_new` (`name_new`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -164,33 +160,44 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`) VALUES
-(1, 'test');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `backbone`
+--
+ALTER TABLE `backbone`
+  ADD CONSTRAINT `bb_creat_id` FOREIGN KEY (`creator`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `entry`
 --
 ALTER TABLE `entry`
   ADD CONSTRAINT `backbone_id` FOREIGN KEY (`backbone`) REFERENCES `backbone` (`id`),
-  ADD CONSTRAINT `creator_id` FOREIGN KEY (`creator`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `insert_id` FOREIGN KEY (`ins`) REFERENCES `ins` (`id`),
-  ADD CONSTRAINT `strain_id` FOREIGN KEY (`strain`) REFERENCES `strain` (`id`);
+  ADD CONSTRAINT `strain_id` FOREIGN KEY (`strain`) REFERENCES `strain` (`id`),
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`creator`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `entry_upstrain`
 --
 ALTER TABLE `entry_upstrain`
-  ADD CONSTRAINT `entry` FOREIGN KEY (`entry_id`) REFERENCES `entry` (`id`);
+  ADD CONSTRAINT `db_id` FOREIGN KEY (`entry_id`) REFERENCES `entry` (`id`);
+
+--
+-- Constraints for table `ins`
+--
+ALTER TABLE `ins`
+  ADD CONSTRAINT `creator_id` FOREIGN KEY (`creator`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `upstrain_file`
+--
+ALTER TABLE `upstrain_file`
+  ADD CONSTRAINT `upstrain_uniqueid` FOREIGN KEY (`upstrain_id`) REFERENCES `entry_upstrain` (`upstrain_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
