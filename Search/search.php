4c5edc360ffad1034586a624e9059875bfeaad5d
Caitlin Haughey <Caitlin@Caitlings-MacBook-Air.local>
2018-02-07 11:59:16 +0100

    .

--- Search/search.php
+++ Search/search.php
@@ -9,8 +9,8 @@
 // connect to database    
 include 'db.php';
 
-    if(isset(mysql_real_escape_string($link, $_REQUEST['submit']))) {
-    
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
         $id_criteria = mysqli_real_escape_string($link, $_REQUEST['id_criteria']);
         $strain_criteria = mysqli_real_escape_string($link, $_REQUEST['strain_criteria']);
         $backbone_criteria = mysqli_real_escape_string($link, $_REQUEST['backbone_criteria']);
@@ -21,31 +21,31 @@ include 'db.php';
         $inserted_date_criteria = mysqli_real_escape_string($link, $_REQUEST['inserted_date_criteria']);
     
         // query the database table
-        $sql="SELECT * FROM entry WHERE (id like '%".$id_criteria."%') OR (backbone like '%".$backbone_criteria."%') OR 
+        $sql ="SELECT * FROM entry WHERE (id like '%".$id_criteria."%') OR (backbone like '%".$backbone_criteria."%') OR 
         (strain like '%".$strain_criteria."%') OR (ins like '%".$insert_criteria."%') OR 
         (entry_reg like '%".$bb_id_criteria."%') OR (comment like '%".$comment_criteria."%') OR
         (year_created like '%".$creation_year_criteria."%') OR
         (date_db like '%".$inserted_date_criteria."%')";
             
-        $result=mysql_query($sql);
-
-        // create a while loop and loop through result set
-        while ($row=mysql_fetch_array($result)) {
-            $name=$row['name'];
-            $ID=$row['ID'];
-        // display the result of the array
-            echo "<ul>\n";
-            echo "<li>" . "<a href=\"search.php?id=$ID\">" . $name . "</a></li>\n";
-            "</ul>";
-            }
+        $result=mysqli_query($link, $sql);
+        
+        if(mysqli_num_rows($result) > 0) {
+            // create a while loop and loop through result set
+            while ($row=mysqli_fetch_array($result, MYSQL_ASSOC)) {
+                echo $row['name'];
+                echo $row['ID'];
+            // display the result of the array
+                echo "<ul>\n";
+                echo "<li>" . "<a href=\"search.php?id=$ID\">" . $name . "</a></li>\n";
+                "</ul>";
+                }
         }
-    else {
-    echo "<p>Please enter a search query</p>";
-    }
-if (mysqli_query($link, $sql)) {
-        echo "<p>Search function failed</p>";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($link);
-    }
-    mysqli_close($link) or die('Could not close connection to database');
+        else {
+            echo "<p>No matching results, try another search.</p>";
+        }
+        }
+    else if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+        header('HTTP/1.0 405 Method Not Allowed');
+    exit;
+}
 
