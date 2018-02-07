6ba4c1e6186b171993816fafff3352a593250953
DESKTOP-TMOJADN\wikto <wiktorg.95@gmail.com>
2018-02-07 11:40:15 +0100

    no message

---
+++ HTML och CSS/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>UpStrain</title>
+	<link href="upstrain.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<?php include 'top.php'; ?>
+	
+	<!-- Main content goes here -->
+	<main>
+		<div class="innertube">
+			<p>main</p>
+		</div>
+	</main>
+	<!-- Site footer -->
+	<footer>
+			<div class="innertube">
+				<p>Footer</p>
+			</div>
+	</footer>
+	
+</body>
+</html>
