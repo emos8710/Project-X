332cd8f43f783146bfe2c472a9ffb02c4f130524
LAPTOP-2FM2P5A2\inaod <ina.oden.osterbo@gmail.com>
2018-02-13 19:24:09 +0100

    hoppas på att det här funkar....

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'upstrain';
-$mysqli = mysqli_connect($host,$user,$pass,$db) or die($mysqli->error);
