d38f3730fd007746e4e1df809d18c79cbedf6e06
Caitlin Haughey <Caitlin@Caitlings-MacBook-Air.local>
2018-02-13 17:06:59 +0100

    Lagt till href till user-page, entry-page oct biobrick.

--- search.php
+++ search.php
@@ -26,22 +26,22 @@
 
             <p>
             <label>Upstrain ID: 
-                <input type="text" name="id_criteria" value="<?php echo $id_criteria;?>"/></label>
+                <input type="text" name="id_criteria"/></label>
             </p>
             
             <p>
             <label>Strain: 
-                <input type="text" name="strain_criteria" value="<?php echo $strain_criteria;?>"/></label>
+                <input type="text" name="strain_criteria"/></label>
             </p>
             
             <p>
             <label>Backbone: 
-                <input type="text" name="backbone_criteria" value="<?php echo $backbone_criteria;?>"/></label>
+                <input type="text" name="backbone_criteria"/></label>
             </p>
             
             <p>    
             <label>Insert: 
-                <input type="text" name="insert_criteria" value="<?php echo $insert_criteria;?>"/></label>
+                <input type="text" name="insert_criteria"/></label>
             </p>
             
             <p>
@@ -54,28 +54,28 @@
             
             <p>    
             <label>Biobrick registry ID: 
-                <input type="text" name="bb_id_criteria" value="<?php echo $bb_id_criteria;?>"/></label>
+                <input type="text" name="bb_id_criteria"/></label>
             </p>
             
             <p>    
             <label>Comment: 
-                <input type="text" name="comment_criteria" ows ="4" cols="50" value="<?php echo $comment_criteria;?>"/></label>
+                <input type="text" name="comment_criteria" ows ="4" cols="50"/></label>
             </p>
             
             <p>    
             <label>Year created: 
                 <input type="text" name="creation_year_criteria" minlength= "4" maxlengh= "4" pattern = "(?:19|20)[0-9]{2})" 
-                placeholder="YYYY" value="<?php echo $creation_year_criteria;?>"/></label>
+                placeholder="YYYY" /></label>
             </p>
             
             <p>    
             <label>Date inserted: 
-                <input type="text" name="inserted_date_criteria" value="<?php echo $inserted_date_criteria;?>"/></label>
+                <input type="text" name="inserted_date_criteria"/></label>
             </p>
             
             <p>    
             <label>Creator: 
-                <input type="text" name="creator_criteria" value="<?php echo $creator_criteria;?>"/></label>
+                <input type="text" name="creator_criteria"/></label>
             </p>
             
             
@@ -105,7 +105,7 @@
         
         if(!empty($id_criteria)) {
             if (!preg_match('/[^A-Za-z0-9]/', $id_criteria)) { 
-                $ConditionArray[] = "t6.upstrain_id = '$id_criteria'";
+                $ConditionArray[] = "t10.upstrain_id = '$id_criteria'";
             } else {
                 echo nl2br ("\n \n Error: Non-valid character usage for 'ID'.");    
             }
@@ -124,7 +124,7 @@
         }    
         
         if(!empty($insert_criteria)) {
-            $ConditionArray[] = "t5.name = '$insert_criteria'";
+            $ConditionArray[] = "t6.name = '$insert_criteria'";
         }   
         
         if(!empty($bb_id_criteria_criteria)) {
@@ -159,19 +159,20 @@
                 $ConditionArray[] = "(t2.first_name = '$creator_criteria' OR t2.last_name = '$creator_criteria')";
         }        
         
-        $entrysql = "SELECT t1.comment AS cmt, t1.year_created AS year, "
+        $entrysql = "SELECT DISTINCT t1.comment AS cmt, t1.year_created AS year, "
 	."t1.date_db AS date, t1.entry_reg AS biobrick, t4.name AS strain, "
-	."t5.name AS ins, t3.name AS backbone, t2.user_id AS user_id, "
-	."t2.first_name AS fname, t2.last_name AS lname, t6.upstrain_id AS up_id "
-        . " FROM entry AS t1 "
+	."GROUP_CONCAT(DISTINCT t6.name SEPARATOR ', ') AS ins, t3.name AS backbone, t2.user_id AS user_id, "
+	."t2.first_name AS fname, t2.last_name AS lname, t10.upstrain_id AS up_id "
+        ."FROM ((ins_type AS t8), (entry AS t1)) "        
         ."INNER JOIN users AS t2 ON t1.creator = t2.user_id " 
         ."INNER JOIN backbone AS t3 ON t1.backbone = t3.id "
         ."INNER JOIN strain AS t4 ON t1.strain = t4.id "
-        ."INNER JOIN ins AS t5 ON t1.ins = t5.id "
-        ."INNER JOIN entry_upstrain AS t6 ON t1.id = t6.entry_id "        
+        ."INNER JOIN entry_inserts AS t5 ON t1.id = t5.entry_id "
+        ."INNER JOIN ins AS t6 ON t8.id = t6.type "        
+        ."INNER JOIN entry_inserts AS t9 ON t6.id = t9.insert_id "              
+        ."INNER JOIN entry_upstrain AS t10 ON t1.id = t10.entry_id "        
         ."WHERE ";     
                 
-        
         if (count($ConditionArray) > 0) {
             $sql = $entrysql . implode(' AND ', $ConditionArray);
             
@@ -179,15 +180,12 @@
             echo nl2br ("\n Error: Please enter search query");
         }
 
-        $result=mysqli_query($link, $sql);  
-
+        $result=mysqli_query($link, $sql);
         
         $iserror = FALSE;
-        if (mysqli_num_rows($result) < 1) {
-            $iserror = TRUE;
-            $error = "No matching results, try another search.";
-        }
-      }
+        
+       mysqli_close($link) or die("Could not close database connection");
+    }
         
     else if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         header('HTTP/1.0 405 Method Not Allowed');
@@ -195,27 +193,43 @@
     }
 	 
 		
-if($iserror && !empty($ConditionArray)) {
-    echo "<h3>Error: ".$error."</h3>";
-    echo "<br>".
-    "<a href=\"javascript:history.go(-1)\">Go back</a>";
-    
-} else {
+    if (!empty($ConditionArray)) {
     echo "<table border='1' cellpadding='5'>";
     echo "<tr><th>upstrain ID</th><th>Strain</th><th>Backbone</th>"
     . "<th>Insert</th><th>Year</th><th>iGEM registry entry</th>"
     . "<th>Creator</th><th>Added date</th><th>Comment</th></tr>";
+    
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["up_id"]. "</td><td>" . $row["strain"]. 
-                "</td><td>" . $row["backbone"]. "</td><td>" . $row["ins"].
-                "</td><td>" . $row["year"]. "</td><td>" . $row["biobrick"]. 
+        if (empty($row["up_id"])) {
+            $iserror = TRUE;
+            $error = "No matching results, try another search.";
+        } else {
+          $biobrick = "";
+          if($row["biobrick"] === null || $row["biobrick"] == ''){ 
+              $biobrick = "N/A";              
+          } else { 
+              $biobrick = "<a class=\"external\" href=\"http://parts.igem.org/Part:".$row["biobrick"]."\" target=\"_blank\">".$row["biobrick"]."</a>"; 
+          }
+            
+        echo "<tr><td><a href=\"entry.php?upstrain_id=". $row["up_id"]."\">".$row["up_id"]."</a>".
+                "</td><td>" . $row["strain"].
+                "</td><td>" . $row["backbone"]. 
+                "</td><td>" . $row["ins"].
+                "</td><td>" . $row["year"]. 
+                "</td><td>" . $biobrick. 
                 "</td><td>" . "<a href=\"user.php?user_id=".$row["user_id"]."\">". 
                 $row["fname"]. " " . $row["lname"]. "</td><td>" . $row["date"].
                 "</td><td>" . $row["cmt"]. "</td></tr>";
+        }
     }
-    echo "</table>";
-    echo $entrydata['cmt'];				
+    echo "</table>";				
+} 
+
+    if ($iserror && !empty($ConditionArray)) {
+        echo "<h3> Error: ".$error."</h3>";
+        echo "<br>".
+        "<a href=\"javascript:history.go(-1)\">Go back</a>";
 }
 ?>
             
