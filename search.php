d82628d21ba34477586d46ab34321d49b6c06e74
Caitlin <Caitlin@Caitlings-MacBook-Air.local>
2018-02-15 17:00:16 +0100

    Fixat buggar för inserts

--- search.php
+++ search.php
@@ -1,9 +1,3 @@
-<?php
-if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_NONE) {
-	session_start();
-}
-?>
-
 <!DOCTYPE html>
 <!--
 
@@ -26,6 +20,10 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
         
         <form action="search.php" method="post" id="searchform">
             
+             <?php
+             //session_start(); 
+             ?>
+
             <p>
             <label>Upstrain ID: 
                 <input type="text" name="id_criteria"/></label>
@@ -58,7 +56,7 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
             
             <p>    
             <label>Biobrick registry ID: 
-                <input type="text" name="bb_id_criteria"/></label>
+                <input type="text" name="bb_id_criteria" placeholder="BBa_K----"/></label>
             </p>
             
             <p>    
@@ -68,13 +66,14 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
             
             <p>    
             <label>Year created: 
-                <input type="text" name="creation_year_criteria" minlength= "4" maxlengh= "4" pattern = "(?:19|20)[0-9]{2})" 
+                <input type="text" name="creation_year_criteria" minlength= "4" maxlengh= "4" pattern = "(?:19|20)[0-9]{2}" 
                 placeholder="YYYY" /></label>
             </p>
             
             <p>    
             <label>Date inserted: 
-                <input type="text" name="inserted_date_criteria"/></label>
+                <input type="text" name="inserted_date_criteria" pattern = "((?:19|20)[0-9]{2})-([0-9]{2})-([0-9]{2})" 
+                       placeholder="YYYY-MM-DD"/></label>
             </p>
             
             <p>    
@@ -132,11 +131,13 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
         }    
         
         if(!empty($insert_criteria)) {
-            $ConditionArray[] = "t6.name = '$insert_criteria'";
+            $ConditionArray[] = "(t9.entry_id IN (SELECT entry_inserts.entry_id FROM "
+                    ."entry_inserts WHERE entry_inserts.insert_id IN (SELECT ins.id FROM "
+                    . "ins WHERE (ins.name = '$insert_criteria'))))";
         }   
         
-        if(!empty($bb_id_criteria_criteria)) {
-            if (!preg_match('/[^A-Za-z0-9]/', $bb_id_criteria)) {
+        if(!empty($bb_id_criteria)) {
+            if (!preg_match('/[^A-Za-z0-9_]/', $bb_id_criteria)) {
                 $ConditionArray[] = "t1.entry_reg = '$bb_id_criteria'";
             } else {
                 $ischarvalid = FALSE;
@@ -158,8 +159,8 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
         } 
         
         if(!empty($inserted_date_criteria)) {
-            if (is_numeric($inserted_date_criteria)) {
-                $ConditionArray[] = "t1.date_db = $inserted_date_criteria";
+            if (!preg_match('/^(?:19|20)[0-9]{2})-([0-9]{2})-([0-9]{2})/', $inserted_date_criteria)) {
+                $ConditionArray[] = "t1.date_db = '$inserted_date_criteria'";
             } else {
                 $ischarvalid = FALSE;
                 echo nl2br ("\n \n Error: Non-valid character usage for 'Date inserted'.");
@@ -173,28 +174,37 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
         } 
         
         if(!empty($insert_type_criteria)) {
-            $ConditionArray[] = "t8.name = '$insert_type_criteria'";
+            $ConditionArray[] = "(t9.entry_id IN (SELECT entry_inserts.entry_id FROM "
+                    . "entry_inserts WHERE entry_inserts.insert_id IN "
+                    . "(SELECT ins.id FROM ins WHERE ins.type IN "
+                    . "(SELECT ins_type.id FROM ins_type WHERE "
+                    . "(ins_type.name = '$insert_type_criteria')))))";
+            
         } 
         
         
         $entrysql = "SELECT DISTINCT t1.comment AS cmt, t1.year_created AS year, "
 	."t1.date_db AS date, t1.entry_reg AS biobrick, t4.name AS strain, "
-	."GROUP_CONCAT(DISTINCT t6.name SEPARATOR ', ') AS ins, t3.name AS backbone, "
-        ."t2.user_id AS user_id, GROUP_CONCAT(DISTINCT t8.name SEPARATOR ', ') AS ins_type, "
-	."t2.first_name AS fname, t2.last_name AS lname, t10.upstrain_id AS up_id "
-        ."FROM ((ins_type AS t8), (entry AS t1)) "        
+	."GROUP_CONCAT(DISTINCT t6.name) AS insname, "
+        ."t3.name AS backbone, "
+        ."t2.user_id AS user_id, "
+        ."GROUP_CONCAT(DISTINCT t7.name) AS instype, "
+	."t2.first_name AS fname, t2.last_name AS lname, "
+        ."t9.upstrain_id AS up_id "
+        ."FROM (entry AS t1) "
+        ."LEFT JOIN entry_inserts AS t5 ON t5.entry_id = t1.id "
+        ."LEFT JOIN ins AS t6 ON t6.id = t5.insert_id "              
+        ."LEFT JOIN ins_type AS t7 ON t7.id = t6.type "        
         ."LEFT JOIN users AS t2 ON t1.creator = t2.user_id " 
         ."LEFT JOIN backbone AS t3 ON t1.backbone = t3.id "
-        ."LEFT JOIN strain AS t4 ON t1.strain = t4.id "
-        ."LEFT JOIN entry_inserts AS t5 ON t1.id = t5.entry_id "
-        ."LEFT JOIN ins AS t6 ON t8.id = t6.type "        
-        ."LEFT JOIN entry_inserts AS t9 ON t6.id = t9.insert_id "              
-        ."LEFT JOIN entry_upstrain AS t10 ON t1.id = t10.entry_id "        
-        ."WHERE ";     
+        ."LEFT JOIN strain AS t4 ON t1.strain = t4.id "                      
+        ."LEFT JOIN entry_upstrain AS t9 ON t1.id = t9.entry_id "                
+        ."WHERE ";
+        
                 
         if (count($ConditionArray) > 0) {
-            $sql = $entrysql . implode(' AND ', $ConditionArray);
-            
+            $sql = $entrysql . implode(' AND ', $ConditionArray) . " GROUP BY up_id";
+                      
         } else if ($ischarvalid && count($ConditionArray) == 0) {
             echo nl2br ("\n Error: Please enter search query");
         }
@@ -234,8 +244,8 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
         echo "<tr><td><a href=\"entry.php?upstrain_id=". $row["up_id"]."\">".$row["up_id"]."</a>".
                 "</td><td>" . $row["strain"].
                 "</td><td>" . $row["backbone"]. 
-                "</td><td>" . $row["ins"].
-                "</td><td>" . $row["ins_type"].
+                "</td><td>" . $row["insname"].
+                "</td><td>" . $row["instype"].
                 "</td><td>" . $row["year"]. 
                 "</td><td>" . $biobrick. 
                 "</td><td>" . "<a href=\"user.php?user_id=".$row["user_id"]."\">". 
@@ -252,8 +262,7 @@ if (session_status() == PHP_SESSION_DISABLED || session_status() == PHP_SESSION_
         "<a href=\"javascript:history.go(-1)\">Go back</a>";
 }
 ?>
-            
-            
+                      
    </main>
         <?php include 'bottom.php'; ?>
     </body>
