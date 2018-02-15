33ace5d286d269a02ac113af2e67c6f7ae61fb4b
LAPTOP-2FM2P5A2\inaod <ina.oden.osterbo@gmail.com>
2018-02-15 14:45:31 +0100

    Fixade success filen (den som säger att man är aktiverad)

--- success.php
+++ success.php
@@ -10,14 +10,15 @@ if (session_status() == PHP_SESSION_NONE) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Success</title>
-	<link href="css/logstyle.css" rel="stylesheet">
 	<link href="css/upstrain.css" rel="stylesheet">
-	<?php include 'top.php'; ?>
 </head>
+
 <body>
+	<?php include 'top.php'; ?>
+<main>
 <div class="form">
-    <h1><?= 'Success'; ?></h1>
-    <p class="loginss">
+    <h1 class="login"><?= 'Success'; ?></h1>
+    <p class="login">
     <?php 
     if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ){
         echo $_SESSION['message'];
@@ -28,6 +29,7 @@ if (session_status() == PHP_SESSION_NONE) {
     ?>
     </p>
 </div>
+</main>
+	<?php include 'bottom.php'; ?>
 </body>
 </html>
-	<?php include 'bottom.php'; ?>
