3dbbde9a9ed4983c19c57d2edd739eeffc29e390
MEANBEANMACHINE\Kristina <kristina.benevides@live.se>
2018-02-15 17:26:00 +0100

    no message

--- insert.php
+++ insert.php
@@ -1,32 +1,133 @@
 
 <?php
 
-include db.php; 
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "upstrain_kristina";
+$link = new mysqli($hostname, $username, $password, $dbname);
 
-$strain= mysqli_real_escape_string($link,$_REQUEST['strain']);
-$backbone= mysqli_real_escape_string($link,$_REQUEST['backbone']);
-$ins = mysqli_real_escape_string($link,$_REQUEST['strain']);
-$ins_type = ""; 
-$reg_link = ""; 
-$year = mysqli_real_escape_string($link,$_REQUEST['strain']);
-$file = ""; 
-$comment = mysqli_real_escape_string($link,$_REQUEST['strain']);
+if ($link->connect_error) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+    exit();
+}
+//
+//else{
+//echo 'Success... ' . mysqli_get_host_info($link) . "\n";
+//}
 
-$sql = "INSERT INTO entry (strain, backbone, year_created, comment) 
-       VALUES (?,?,?,?)"; 
-$stmt = mysqli_prepare($sql); 
-$stmt->bind_param("ssss",$strain, $backbone, $year, $comment); 
-$stmt->execute();
-$stmt->close(); 
+// Variables
 
-if (mysqli_query($link, $sql)) {
-    echo "New entry created successfully";
+$year = $_REQUEST['year'];
+$comment = $_REQUEST['comment'];
+$strain = $_REQUEST['strain'];
+$backbone = $_REQUEST['backbone'];
+$ins = $_REQUEST['ins'];
+
+
+$reg_link = mysqli_real_escape_string($link, $_REQUEST['registry']);
+;
+
+$ins_type = "";
+$file = "";
+$bb_reg = "";
+$creator = "";
+
+$current_date = date("Y-m-d");
+
+// Strain 
+$check = "SELECT name FROM strain WHERE name LIKE '$strain'";
+$check_query = mysqli_query($link, $check);
+if (mysqli_num_rows($check_query) < 1) {
+    $sql_strain = "INSERT INTO strain (name) VALUES (?)";
+    $stmt_strain = $link->prepare($sql_strain);
+    $stmt_strain->bind_param("s", $strain);
+    $stmt_strain->execute();
+    echo "Strain inserted into strain successfully";
+    $stmt_strain->close();
+
+    $strain_s = "SELECT id FROM strain WHERE name LIKE '$strain'";
+    $strain_s_query = mysqli_query($link, $strain_s);
+    $strain_row = mysqli_fetch_assoc($strain_s_query);
+    $strain_row_id = $strain_row["id"];
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($link);
+
+    $strain_s = "SELECT id FROM strain WHERE name LIKE '$strain'";
+    $strain_s_query = mysqli_query($link, $strain_s);
+    $strain_row = mysqli_fetch_assoc($strain_s_query);
+    $strain_row_id = $strain_row["id"];
+
+    $strain_id = "INSERT INTO entry (strain) VALUES (?)";
+    $stmt_strainid = $link->prepare($strain_id);
+    $stmt_strainid->bind_param("s", $strain_row_id);
+    $stmt_strainid->execute();
+    $stmt_strainid->close();
 }
 
-mysqli_close($link) or die('Could not close connection to database');
 
-?>
+// Backbone
+$check2 = "SELECT name FROM backbone WHERE name LIKE '$backbone'";
+$check_query2 = mysqli_query($link, $check2);
+if (mysqli_num_rows($check_query2) < 1) {
+    $sql_backbone = "INSERT INTO backbone (name, date_db) VALUES (?,?)";
+    $stmt_backbone = $link->prepare($sql_backbone);
+    $stmt_backbone->bind_param("ss", $backbone, $current_date);
+    $stmt_backbone->execute();
+    echo "Backbone inserted into backbone successfully";
+    $stmt_backbone->close();
+
+    $back_s = "SELECT id FROM backbone WHERE name LIKE '$backbone'";
+    $back_s_query = mysqli_query($link, $back_s);
+    $back_row = mysqli_fetch_assoc($back_s_query);
+    $back_row_id = $back_row["id"];
+} else {
+    $back_s = "SELECT id FROM backbone WHERE name LIKE '$backbone'";
+    $back_s_query = mysqli_query($link, $back_s);
+    $back_row = mysqli_fetch_assoc($back_s_query);
+    $back_row_id = $back_row["id"];
+
+    $back_id = "INSERT INTO entry (backbone) VALUES (?)";
+    $stmt_backid = $link->prepare($back_id);
+    $stmt_backid->bind_param("s", $back_row_id);
+    $stmt_backid->execute();
+    $stmt_backid->close();
+}
+
+// Entry
+$sql_entry = "INSERT INTO entry (year_created, comment, date_db, backbone, strain) VALUES (?,?,?,?,?)";
+$stmt_entry = $link->prepare($sql_entry);
+$stmt_entry->bind_param("sssss", $year, $comment, $current_date, $back_row_id, $strain_row_id);
+$stmt_entry->execute();
+echo "Year, comment, date_db inserted into entry successfully";
+$stmt_entry->close();
+
+// Insert 
+//if(isset($_POST['submit'])) {
+   
+//if ($_POST['ins']) {
+    //foreach ($_POST['ins'] as $key=>$value) {
+            //$sql_ins = "INSERT INTO ins (name,date_db) VALUES (?,?)";
+            //$stmt_ins = $link->prepare($sql_ins);
+            //->bind_param("ss", $value, $current_date);
+            //->execute();
+            //->close();
+       // }
+    //echo "Insert inserted into ins successfully"; 
+    //}
+//} else {
+    //echo "No inserts given";
+//}
+
+    
+//
+
+
+//Reg link 
+
+//Sequence 
+
+
+$link->close() or die('Could not close connection to database');
+
 
 
--- new_insert.php
+++ new_insert.php
@@ -3,146 +3,204 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>New Entry</title>
-	<link href="css/upstrain.css" rel="stylesheet">
+    <title>New Entry</title>
+    <link href="css/upstrain.css" rel="stylesheet">
+
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type = "text/javascript" src = "http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>    
+
 </head>
 
 <body>
-	
-	<?php include 'top.php'; ?>
-	
-	<!-- Main content goes here -->
-	<main>
-		<div class="innertube">
-			<h2>New Entry</h2>
-            
-        <?php
-        $strainErr = $backboneErr = $yearErr = $regErr = ""; 
-        $strain = $backbone = $comment = $year = $private = $reg = ""; 
-        
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-           
-            if (empty($_POST["strain"])) {
-                 $strainErr = "A strain is required"; 
-            } else {
-                 $strain = test_input($_POST["strain"]); 
-            }
-            
-            if(empty($_POST["backbone"])) {
-                $backboneErr = "A backbone is required"; 
-            } else {
-                $backbone = test_input($_POST["backbone"]); 
-            }           
-            
-            if(empty($_POST["comment"])) {
-                $comment = ""; 
-            } else {
-                $comment = test_input($_POST["comment"]); 
-            }
-            
-            if(empty($_POST["year"])) {
-                $yearErr = "A year is required";  
-            } elseif(!is_numeric($year)) {
-                $yearErr = "The input year is not valid";  
-            } else {
-                $year = test_input($_POST["year"]); 
-            }
-            
-            $reg = test_input($_POST["reg_link"]); 
-        
-        }
-        
-        
-        function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-             return $data; 
-            
-        }
-        ?>
-        
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["insert.php"]);?>" >
-           
+
+    <?php include 'top.php'; ?>
+
+    <!-- Main content goes here -->
+    <main>
+        <div class="innertube">
+            <h2>New Entry</h2>
+
             <?php
-             session_start(); 
-             ?>
-             
-             <p>
-                <label for="strain">Strain </label>
-                <input type="text" name="strain" id="strain" value="<?php echo $strain;?>" />
-                <span class="error">* <?php echo $strainErr; ?></span>
-                <br/></p>
-            
-             <p>
-                <label for="backbone">Backbone </label>
-                <input type="text" name ="backbone" id="backbone" value="<?php echo $backbone;?>" /> 
-                <span class="error">* <?php echo $backboneErr; ?></span>
-                <br/></p>
-            
-            <p> <label for="insert">Insert </label>
-                <input type='text' name="insert" value="" class="auto"></p>
-            </p>
-        
-             <p>
-               <select name="insert_type">
-                <option value="promotor">Promotor</option>
-                <option value="coding_seq">Coding sequence</option>
-                <option value="RBS">RBS</option>
-                <option value="other">Other</option>
-            </select></p>
-            
-            <p> 
-                <label for="reg_link">Registry link</label>
-                <input type="text" name="registry" id="reg" value="<?php echo $reg;?>" /> 
-            </p>
-            
-            <p> 
-            <label for="seq">Sequence </label>
-            <input type="file" name="fileToUpload" id="fileToUpload">
-            
-            </p>
-            <p>
-                <label for="year">Year </label>
-                <input type="text" name = "year" id="year" minlength= "4" maxlengh= "4" pattern = "(?:19|20)[0-9]{2})" 
-                placeholder="YYYY" value="<?php echo $year; ?>" /> </p>
-            
-           <p> 
-                <label for="comment">Comment </label>
-                <input type="textarea" name="comment" id="comment" rows ="4" cols="50"
-                value="<?php echo $comment;?>" /> </p>
-            
-           <p>
-               <label for="private">Make this entry private </label>
-               <input type="checkbox" name="private" value="private" </p>
-           
-           <p id="submit">
-               <input type="submit" value="Submit" />
-        
-             </p>
-            
-        </form>
-		
-			</div>
-        
-        
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>    
-<script type="text/javascript">
-$(function() {
-    
-    //autocomplete
-    $(".auto").autocomplete({
-        source: "search.php",
-        minLength: 1
-    });                
-});
-</script>
-        
-	</main>
-	
-	<?php include 'bottom.php'; ?>
-	
+            $strainErr = $backboneErr = $yearErr = "";
+            $strain = $backbone = $comment = $year = $reg = $inst = "";
+
+            if ($_POST) {
+
+                if (empty($_POST["strain"])) {
+                    $strainErr = "A strain is required";
+                } else {
+                    $strain = test_input($_POST["strain"]);
+                }
+
+                if (empty($_POST["backbone"])) {
+                    $backboneErr = "A backbone is required";
+                } else {
+                    $backbone = test_input($_POST["backbone"]);
+                }
+
+
+                if (empty($_POST["year"])) {
+                    $yearErr = "A year is required";
+                } elseif (!is_numeric($year)) {
+                    $yearErr = "The input year is not valid";
+                } else {
+                    $year = test_input($_POST["year"]);
+                }
+
+                if (empty($_POST["comment"])) {
+                    $comment = "";
+                } else {
+                    $comment = test_input($_POST["comment"]);
+                }
+
+                if (empty($_POST["registry"])) {
+                    $reg = "";
+                } else {
+                    $reg = test_input($_POST["registry"]);
+                }
+
+                if (empty($_POST["ins"])) {
+                    $inst = "";
+                } else {
+                    $inst = test_input($_POST["ins"]);
+                }
+            }
+
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+            ?>
+
+            <form method="post" action="<?php echo htmlspecialchars("insert.php"); ?>" >
+
+                <p>
+                    <label for="Strain">Strain </label>
+                    <input type="text" name="strain" id="Strain" value="<?php echo $strain; ?>" required/>
+                    <span class="error">* <?php echo $strainErr; ?></span>
+                    <br/></p>
+
+                <p>
+                    <label for="Backbone">Backbone </label>
+                    <input type="text" name ="backbone" id="Backbone" value="<?php echo $backbone; ?>" required/> 
+                    <span class="error">* <?php echo $backboneErr; ?></span>
+                    <br/></p>
+
+                <p> <table id="dynamic">
+                    <label for="Ins">Insert </label>
+                    <input type='text' name="ins[]" value="<?php echo $inst; ?>" id ="Ins" class="auto" multiple/>
+                    <button type="button" name="add" id="add_input">+ Add insert</button>
+                </table>
+                </p>
+
+                </p>
+
+
+                <p>
+                    <label for="Ins_Type">Insert type </label>
+                    <select name="insert_type">
+                        <option value="promotor">Promotor</option>
+                        <option value="coding_seq">Coding sequence</option>
+                        <option value="RBS">RBS</option>
+                        <option value="other">Other</option>
+                    </select></p>
+
+                <p> 
+                    <label for="Registry">Registry id</label>
+                    <input type="text" name="registry" id="Registry" value="<?php echo $reg; ?>" placeholder ="BBa_K[X]" /> 
+                </p>
+
+                <p> 
+                    <label for="FileToUpload">Sequence </label>
+                    <input type="file" name="fileToUpload" id="FileToUpload">
+
+                </p>
+                <p>
+                    <label for="Year">Year </label>
+                    <input type="text" name = "year" id="Year" minlength= "4" maxlengh= "4" pattern = "(?:19|20)[0-9]{2})" 
+                           placeholder="YYYY" value="<?php echo $year; ?>" required/>
+                    <span class="error">* <?php echo $yearErr; ?></span>
+                    <br/></p>
+                <p> 
+                    <label for="Comment">Comment </label>
+                    <textarea name="comment" id="Comment" rows ="4" cols="50"
+                              value="<?php echo $comment; ?>" > </textarea> </p>
+
+                <p>
+                    <label for="Private">Make this entry private </label>
+                    <input type="checkbox" name="private" value="Private" </p>
+
+                <p id="submit">
+                    <input type="submit" value="Submit" />
+
+                </p>
+
+            </form>
+
+
+
+        </div>
+
+
+
+
+
+    </main>
+
+    <?php include 'bottom.php'; ?>
+
 </body>
 </html>
+
+<script>
+    $(document).ready(function() {
+        var i = 1;
+        var max = 5; 
+        $("#add_input").click(function() {
+            if(i <= max) {
+               $("#dynamic").append('<tr id="row' + i + '"><td><input type="text" name="ins[]" id ="Ins" multiple/></td><td><button type="button" name="remove" id="' + i + '" class="btn_remove">Remove insert</button></td></tr>'); 
+               i++;
+               
+            } else {
+           
+            }
+                
+        });
+        $(document).on('click', '.btn_remove', function() {
+            var button_id = $(this).attr("id");
+            $("#row" + button_id + '').remove();
+            i--;  
+
+        });
+        $('#submit').click(function() {
+            $.ajax({
+                url: "insert.php",
+                method: "POST",
+                data: $('#add_me').serialize(),
+                success: function(data)
+                {
+                    alert(data);
+                    $('#add_me')[0].reset();
+                }
+            });
+        });
+    });
+
+</script>
+
+<script type="text/javascript">
+    $(function () {
+
+        //autocomplete
+        $(".auto").autocomplete({
+            source: "autocomplete_ins.php",
+            minLength: 1
+        });
+    });
+</script>
+
+
 
