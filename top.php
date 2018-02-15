3298d7c56bbe792abd6758c3cc3f0cc1936b865a
Emma-laptop <kottdjur@hotmail.com>
2018-02-15 12:15:53 +0100

    no message

--- top.php
+++ top.php
@@ -20,87 +20,87 @@ else {
 	<!-- Logo -->
 	<div class="logo">
 		<h1>UpStrain</h1>
-		<p1>The plasmid database for iGEM Uppsala</p1>
+		<p>The plasmid database for iGEM Uppsala</p>
 	</div>
 	
 	<!-- NAVIGATION BUTTONS -->
-		<!-- Home -->
-		<a <?php 
-				if(basename($_SERVER['PHP_SELF']) == "index.php"){
-					echo "class=\"active\" ";
-				} 
-			?> href="index.php">Home</a>
-			
-		<!-- Search -->
-		<a <?php 
-				if(basename($_SERVER['PHP_SELF']) == "search.php"){
-					echo "class=\"active\" ";
-				} 
-			?> href="search.php">Search</a>
+	<!-- Home -->
+	<a <?php 
+			if(basename($_SERVER['PHP_SELF']) == "index.php"){
+				echo "class=\"active\" ";
+			} 
+		?> href="index.php">Home</a>
 		
-		<!--  New Entry -->
-		<a <?php 
-				if(basename($_SERVER['PHP_SELF']) == "new_insert.php"){
-					echo "class=\"active\" ";
-				} 
-			?> href="new_insert.php">New entry</a>
+	<!-- Search -->
+	<a <?php 
+			if(basename($_SERVER['PHP_SELF']) == "search.php"){
+				echo "class=\"active\" ";
+			} 
+		?> href="search.php">Search</a>
+	
+	<!--  New Entry -->
+	<a <?php 
+			if(basename($_SERVER['PHP_SELF']) == "new_insert.php"){
+				echo "class=\"active\" ";
+			} 
+		?> href="new_insert.php">New entry</a>
+		
+	<!-- Help -->
+	<a <?php 
+			if(basename($_SERVER['PHP_SELF']) == "help.php"){
+				echo "class=\"active\" ";
+			} 
+		?> href="help.php">Help</a>
+		
+	<!-- Profile (if logged in) -->
+	<?php
+	if($loggedin) { ?>
+		<a <?php
+			if(basename($_SERVER['REQUEST_URI']) == "user.php?user_id=".$_SESSION['user_id'] || basename($_SERVER['REQUEST_URI']) == "user.php?user_id=".$_SESSION['user_id']."&edit=1") {
+				echo "class=\"active\" ";
+			} 
+			?> href="user.php?user_id=<?php echo $_SESSION['user_id']; ?>">My Profile</a>
+	<?php
+	}
+	?> 
 			
-		<!-- Help -->
-		<a <?php 
-				if(basename($_SERVER['PHP_SELF']) == "help.php"){
-					echo "class=\"active\" ";
-				} 
-			?> href="help.php">Help</a>
-			
-		<!-- Profile (if logged in) -->
-		<?php
-		if($loggedin) { ?>
-			<a <?php
-				if(basename($_SERVER['REQUEST_URI']) == "user.php?user_id=".$_SESSION['user_id'] || basename($_SERVER['REQUEST_URI']) == "user.php?user_id=".$_SESSION['user_id']."&edit=1") {
-					echo "class=\"active\" ";
-				} 
-				?> href="user.php?user_id=<?php echo $_SESSION['user_id']; ?>">My Profile</a>
-		<?php
-		}
-		?> 
-				
-		<!-- Control Panel (if admin) -->
-		<?php if($admin) {
-			?>
-			
-			<a <?php
-					if(basename($_SERVER['PHP_SELF'])=="control_panel.php") {
-						echo "class=\"active\"";
-					}
-				?> href="control_panel.php">Control Panel</a>
-		<?php 
-		}
+	<!-- Control Panel (if admin) -->
+	<?php if($admin) {
 		?>
 		
-		<!-- Login -->
-		<?php			
-			if($loggedin) {
+		<a <?php
+				if(basename($_SERVER['PHP_SELF'])=="control_panel.php") {
+					echo "class=\"active\"";
+				}
+			?> href="control_panel.php">Control Panel</a>
+	<?php 
+	}
+	?>
+	
+	<!-- Login -->
+	<?php			
+		if($loggedin) {
+	?>
+			<a class="login" href="logout.php">Log out</a>
+	<?php 
+		}
+		else{ 
+	?>
+			<a class="login 
+			<?php 
+				if(basename($_SERVER['PHP_SELF'])=="logsyst.php"){
+					echo " active";
+			}?>" href="logsyst.php">Log in</a>	
+			<?php
+		}
 		?>
-				<a class="login" href="logout.php">Log out</a>
-		<?php 
-			}
-			else{ 
-		?>
-				<a class="login 
-				<?php 
-					if(basename($_SERVER['PHP_SELF'])=="logsyst.php"){
-						echo " active";
-				}?>" href="logsyst.php">Log in</a>	
-				<?php
-			}
-			?>
 	
 	
 	<!-- Quick search -->
 	<div class="quicksearch">
 		<form action="entry.php">
 			<input type="text" placeholder="Search UpStrain ID" name="upstrain_id"></input>
-			<button type="submit"><img class="quicksearch" src="images/search_button.png"></img></button>
+			<button class="quicksearch" type="submit"><img class="quicksearch" src="images/search_button.png"></img></button>
 		</form>
 		<a href="search.php">Advanced search</a>
 	</div>
