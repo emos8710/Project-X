d2e33848087aa9fa9afa09bf412942322336ecc1
LAPTOP-2FM2P5A2\inaod <ina.oden.osterbo@gmail.com>
2018-02-15 16:06:05 +0100

    Password check i reset_password.

--- reset_password.php
+++ reset_password.php
@@ -7,30 +7,35 @@ if (session_status() == PHP_SESSION_NONE) {
 
 // Make sure the form is being submitted with method="post"
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
-
-    // Make sure the two passwords match
-    if ( $_POST['newpassword'] == $_POST['confirmpassword'] ) { 
-
-        $new_password = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+	// Checks that the password is long enough
+	if (!strlen($_POST['newpassword'])<8) {
+		// Make sure the two passwords match
+		if ( $_POST['newpassword'] == $_POST['confirmpassword'] ) { 
+	
+			$new_password = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
         
-        // We get $_POST['email'] and $_POST['hash'] from the hidden input field of reset.php form
-        $email = $mysqli->escape_string($_POST['email']);
-        $hash = $mysqli->escape_string($_POST['hash']);
+			// We get $_POST['email'] and $_POST['hash'] from the hidden input field of reset.php form
+			$email = $mysqli->escape_string($_POST['email']);
+			$hash = $mysqli->escape_string($_POST['hash']);
         
-        $sql = "UPDATE users SET password='$new_password', hash='$hash' WHERE email='$email'";
+			$sql = "UPDATE users SET password='$new_password', hash='$hash' WHERE email='$email'";
 
-        if ( $mysqli->query($sql) ) {
+			if ( $mysqli->query($sql) ) {
 
-        $_SESSION['message'] = "Your password has been reset successfully!";
-        header("location: success.php");    
+			$_SESSION['message'] = "Your password has been reset successfully!";
+			header("location: success.php");    
 
-        }
+			}
 
-    }
-    else {
-        $_SESSION['message'] = "Two passwords you entered don't match, try again!";
-        header("location: error.php");    
-    }
+		}
+		else {
+			$_SESSION['message'] = "Two passwords you entered don't match, try again!";
+			header("location: error.php");    
+		}
 
-}
+	}
+	else {
+		$_SESSION['message']="The new password is too short! Please try again. The password must be at least 8 characters long.";
+		header("location: error.php");
+	}
 ?>
