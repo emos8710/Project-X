9f60e4fafd6940c4a64124f6281b59b616d5a0f2
LAPTOP-2FM2P5A2\inaod <ina.oden.osterbo@gmail.com>
2018-02-20 13:08:20 +0100

    no message

--- top.php
+++ top.php
@@ -4,10 +4,12 @@ if (count(get_included_files()) == 1) exit("Access restricted.");
 
  /* Logs out user if no activity in a certain time (at the moment 2 minutes) */ 
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true && isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > (20))) {
-    $_SESSION['logged_in']=false;
-	header("Refresh:0; url=logout.php");
-	session_unset();     // unset $_SESSION variable for the run-time 
-    session_destroy();   // destroy session data in storage
+    if !(basename($_SERVER['PHP_SELF']=="logout.php")) {
+		$_SESSION['logged_in']=false;
+		header("Refresh:0; url=logout.php");
+		session_unset();     // unset $_SESSION variable for the run-time 
+		session_destroy();   // destroy session data in storage
+	}
 }
 else {
 	$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
