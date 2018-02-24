311eab40044e249457c43632295d97bf9475cc2c
DESKTOP-TMOJADN\wiktor <wiktorg.95@gmail.com>
2018-02-24 12:19:49 +0100

    Mer petande i felhantering

--- user.php
+++ user.php
@@ -5,22 +5,19 @@
 	
 	//Function for parsing URL variable (making sure user ID is proper)
 	function check_user_id($input) {
-	if (preg_match('/^\d+$/', $input) == 1): return TRUE; else: return FALSE; endif;
+	return preg_match('/^\d+$/', $input) == 1;
 	}
 	
-	$is_user_error = FALSE;
-	$is_mysql_error = FALSE;
+	$is_user_error = !isset($_GET['user_id']);
 	
-	if (!isset($_GET['user_id'])) {
-		$is_user_error = TRUE;
+	if ($is_user_error) {
 		$user_error = "No user id specified.";
 	} else {
-		
 		// Fetch the user id from URL
 		$user_id = $_GET['user_id'];
 		
-		if(!check_user_id($user_id)) {
-			$is_user_error = TRUE;
+		$is_user_error = !check_user_id($user_id);
+		if($is_user_error) {
 			$user_error = "Invalid user ID.";
 		}
 	}
@@ -28,48 +25,45 @@
 	if (!$is_user_error) {
 	
 		// Connect to database
-		include 'scripts\db.php';
+		include 'scripts/db.php';
 		
 		// Check if user exists
 		$id = mysqli_real_escape_string($link, $user_id);
 		$sql = "SELECT user_id, username AS uname FROM users WHERE user_id LIKE '$id'";
 		$result = mysqli_query($link, $sql);
 
-		if(!$result) {
-			$is_mysql_error = TRUE;
-			$mysql_error = mysqli_error();
-		} elseif(mysqli_num_rows($result) < 1) {
-			$is_mysql_error = TRUE;
-			$mysql_error = "No such user";
-		} elseif(mysqli_num_rows($result) > 1) {
-			$is_mysql_error = TRUE;
-			$mysql_error = "This should never happen";
+		$is_mysql_error = !$result;
+		if($is_mysql_error) {
+			$mysql_error = mysqli_error($link);
+		} else {
+			$is_mysql_error = mysqli_num_rows($result) < 1;
+			if ($is_mysql_error) {
+				$mysql_error = "No such user";
+			} else {
+				$is_mysql_error = mysqli_num_rows($result) > 1;
+				if ($is_mysql_error) $mysql_error = "This should never happen";
+			}
 		}
 		
-		$username = mysqli_fetch_assoc($result)['uname'];
+		if (!$is_mysql_error) {
+			$username = mysqli_fetch_assoc($result)['uname'];
+		}
+		
 		mysqli_free_result($result);
 		
 		// Close database connection
 		mysqli_close($link) or die("Could not close database connection");
 		
-		if (isset($_GET["edit"])) {
-			$edit = TRUE;
-		} else {
-			$edit = FALSE;
-		}
+		$edit = isset($_GET['edit']);
 		
-		if(isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id) {
-			$isowner = TRUE;
-		}
-		else {
-			$isowner = FALSE;
-		}
+		$isowner = (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id);
+		
 	}
 	
 	if($is_user_error) {
-		$title = "User ID error";
+		$title = "Error: ".$user_error;
 	} else if($is_mysql_error) {
-		$title = "Database error";
+		$title = "Error: ".$mysql_error;
 	} else {
 		$title = "User ".$username;
 	}
@@ -93,15 +87,16 @@
 			
 			// Print error text...
 			if ($is_user_error || $is_mysql_error) {
-				if($is_user_error) echo "<h3>Error: ".$user_error."</h3><br>";
-				if($is_mysql_error) echo "<br><h3>Error: ".$mysql_error."</h3>";
+				if ($is_user_error): echo "<h3>Error: ".$user_error."</h3>";
+				elseif ($is_mysql_error): echo "<br><h3>Error: ".$mysql_error."</h3>";
+				endif;
 				echo "<br>".
 				"<a href=\"javascript:history.go(-1)\">Go back</a>";
 			// ... Or show user information or edit page
 			} else {
 				
 				// Connect to database
-				include 'scripts\db.php';
+				include 'scripts/db.php';
 				
 				// Fetch user information from database
 				$usersql = "SELECT first_name AS fname, last_name AS lname, "
