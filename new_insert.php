af3e1698385344ce199d0b4f82d490cd05430906
MEANBEANMACHINE\Kristina <kristina.benevides@live.se>
2018-03-06 16:49:06 +0100

    no message

--- new_insert.php
+++ new_insert.php
@@ -94,19 +94,8 @@ $title = "New entry";
                 }
                 mysqli_close($link);
             }
-            
-            function load_ins_name() {
-                include 'scripts/db.php'; 
 
-$out = '';
-$sql = "SELECT id,name from ins WHERE type='" . $_POST['inst'] . "' ORDER BY name";
-$res = mysqli_query($link, $sql);
-$out = '<option value="">Select insert name</option>';
-while ($row = mysqli_fetch_assoc($res)) {
-    $out .= '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
-}
-echo $out;
-            }
+            
             ?>
 
             <h2>New Entry</h2>
