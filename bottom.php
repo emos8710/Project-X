dd869dc8a84e09777e9bf0328f80b6e0a06c7082
Emma-laptop <kottdjur@hotmail.com>
2018-03-07 12:54:54 +0100

    Footer, inte helt klar ännu

--- bottom.php
+++ bottom.php
@@ -3,7 +3,22 @@
 <!-- Site footer -->
 <footer>
     <div class="innertube">
+		<img src="images/uplogo_notext.png" alt="UpStrain logo" height="45"></img>
+		<div>
+			<h4>External links</h4>
+			<br><a href="http://igemuppsala.se/">iGEM Uppsala</a>
+			<br><a href="http://igem.org/Main_Page">iGEM Main Page</a>
+			<br><a href="http://igem.org/Main_Page">iGEM Registry</a>
+		</div>
+		<div>
+			<h4>Navigation</h4>
+			<br><a href="index.php">Home</a>
+			<br><a href="help.php">Help</a>
+			<br><a href="search.php">Search</a>
+		</div>
+			
         <p>No actual copyright 2018. No rights really reserved.</p>
+		
     </div>
 </footer>
 </body>
